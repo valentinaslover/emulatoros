d75d00614e9e7491c8f3facc02187eccd180529e
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-04-02 00:18:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -42,7 +42,25 @@ try {
 } catch {
   //empty b/c no need for return
 }
+function requestFullScreen(element) {
+    // Supports most browsers and their versions.
+    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullscreen;
 
+    if (requestMethod) { // Native full screen.
+        requestMethod.call(element);
+    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
+        }
+    }
+}
+
+function makeFullScreen() {
+    document.getElementsByTagName("iframe")[0].className = "fullScreen";
+    var elem = document.body;
+    requestFullScreen(elem);
+}
 //Hamburger Menu Navbar
 const toggleMenu = () => {
   $("#hamburgerMenu").toggleClass("collapsed");
