3a5390aa75b94e4dff0a00c2c12a9f3733c04273
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-04-20 15:58:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -86,7 +86,7 @@ $header.innerHTML = `<nav class="aa-nav">
 </div>
 <div class="aa-nav-items">
   <span class="aa-nav-item"><a href="https://emulatoros.github.io" target="_top">Home</a></span>
-  <span class="aa-nav-item"><a href="https://emulatoros.github.io/Quick-Links.html" target="_top">Quick Links</a></span>
+  
   <span class="aa-nav-item"><a href="https://emulatoros.github.io" target="_top">Games</a></span>
   <span class="aa-nav-item"><a href="/emulatorjs/index.html" target="_top">EmulatorJS</a></span>
      <span class="aa-nav-item"><a href="/tests.html" target="_top">Tests</a></span>
