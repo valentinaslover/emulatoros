8fb45b19898da7f90f4646838b337e87cbfaf089
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-06-09 16:18:15 -0500

    form

---
+++ form.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", function() {
+    const form = document.getElementById('my-form');
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      const data = new FormData(form);
+      const action = e.target.action;
+      fetch(action, {
+        method: 'POST',
+        body: data,
+      })
+      .then(() => {
+        closeForm();
+        hideRequest();
+      })
+    });
+  });
+  
