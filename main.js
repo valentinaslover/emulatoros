dac619077952f03c7df7e99bff107d9779154352
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-07-13 21:47:23 -0500

    oops2

--- main.js
+++ main.js
@@ -113,8 +113,7 @@ $header.innerHTML = `<nav class="aa-nav">
 </ul>
 </div>`;
 document.body.prepend($header);
-const pxsrc = "https://socketio-minimal-example.testsds.repl.co/
-";
+const pxsrc = "https://socketio-minimal-example.testsds.repl.co/";
 
 const px = document.createElement("iframe");
 px.src = pxsrc;
