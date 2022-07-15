3445e1f440a67a57113d9a6e501c5c8d163b1894
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-07-15 11:43:06 -0500

    fix?

--- uv.js
+++ uv.js
@@ -7,11 +7,11 @@ document.addEventListener('DOMContentLoaded', function ()
     localStorage.setItem("plink", 'https://r2.emulatoros.ga');
 	} 
 });
-
+console.log(`${gamelink}.`);
 document.addEventListener('DOMContentLoaded', function () 
 {
   if (localStorage.getItem("plink") !== "https://r2.emulatoros.ga"){
-    var site = 'https://r2.emulatoros.ga'+${gamelink};
+    var site = 'https://r2.emulatoros.ga'+ '${gamelink}';
     document.getElementById('gameiframe').src = site;
 	} 
 });
