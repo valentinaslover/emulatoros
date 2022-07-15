a158bf67ac3947638d581cee39050fb61562ec51
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-07-15 10:35:45 -0500

    fix

--- uv.js
+++ uv.js
@@ -1,13 +1,12 @@
 const game = document.querySelector("#gameiframe");
 const gamelink = game.dataset.gamelink;
 
-var proxlink = localStorage.getItem("plink");
-
 document.addEventListener('DOMContentLoaded', function () 
 {
   if (localStorage.getItem("plink") !== ""){
     localStorage.setItem("stylepallet", "r2.emulatoros.ga");
 	} 
 });
+var proxlink = localStorage.getItem("plink");
 
-document.getElementById("iframesrc").src = "proxlink/${gamelink}";
+document.getElementById("iframesrc").src = proxlink;
