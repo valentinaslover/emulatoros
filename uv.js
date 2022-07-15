9cc3fae5ab0306c2de61ab5f28f3dcdbf27ab7be
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-07-15 12:03:28 -0500

    log

--- uv.js
+++ uv.js
@@ -4,17 +4,19 @@ const gamelink = game.dataset.gamelink;
 document.addEventListener('DOMContentLoaded', function () 
 {
   if (localStorage.getItem("plink") !== ""){
-    localStorage.setItem("plink", 'https://r2.emulatoros.ga');
+    localStorage.setItem("plink", 'r2');
 	} 
 });
 console.log(`${gamelink}.`);
+localStorage.setItem("plink", 'r2');
 document.addEventListener('DOMContentLoaded', function () 
 {
-  if (localStorage.getItem("plink") !== "https://r2.emulatoros.ga"){
+  if (localStorage.getItem("plink") !== "r2"){
     setTimeout(function(){
         var site = 'https://r2.emulatoros.ga'+ '${gamelink}';
 
     document.getElementById('gameiframe').src = site;
+    console.log(`change link`);
     }, 1000);
     
 	} 
