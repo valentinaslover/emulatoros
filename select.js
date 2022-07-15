e542137666629ffd385fbf9bac4a2c79ad6b2b6f
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-07-15 16:35:17 -0500

    hide

--- select.js
+++ select.js
@@ -6,3 +6,9 @@ function pselectio(lin)
 {
   localStorage.setItem("plink", lin)
 }
+document.addEventListener('DOMContentLoaded', function(){
+    if (localStorage.getItem("particletheme") !== "hide"){
+        
+    console.log('nopRT');
+    }
+    });
