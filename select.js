2fef21f648152a5a12a45eb54dd69b9708c0f447
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-07-15 17:37:53 -0500

    try

--- select.js
+++ select.js
@@ -7,8 +7,32 @@ function pselectio(lin)
   localStorage.setItem("plink", lin)
 }
 document.addEventListener('DOMContentLoaded', function(){
-    if (localStorage.getItem("particletheme") !== "hide"){
+    if (localStorage.getItem("particletheme") !== 'hide'){
         
     console.log('nopRT');
     }
     });
+    document.addEventListener('DOMContentLoaded', function(){
+    if (localStorage.getItem("particletheme") !== 'blue'){
+        
+    console.log('nopRTB');
+    }
+    });
+    document.addEventListener('DOMContentLoaded', function(){
+        if (localStorage.getItem("particletheme") !== 'normal3'){
+            
+        console.log('nopRT3');
+        }
+        });
+        document.addEventListener('DOMContentLoaded', function(){
+            if (localStorage.getItem("particletheme") !== 'normal2'){
+                
+            console.log('nopRT2');
+            }
+            });
+            document.addEventListener('DOMContentLoaded', function(){
+                if (localStorage.getItem("particletheme") !== 'normal1'){
+                    
+                console.log('nopRT1');
+                }
+                });
