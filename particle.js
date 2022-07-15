9a5d7e3ef60bbfa1cf32f20e0e37ec2a35556074
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-07-14 21:39:12 -0500

    maybe fix

--- particle.js
+++ particle.js
@@ -1,8 +1,5 @@
-
-  const gamewrap = document.querySelector("#bodyjs");
-  console.log('normal');
-  const ejsscript = document.createElement("script");
-  ejsscript.text = `var count_particles, stats, update; particlesJS("particles-js", { particles: { number: { value: 47, density: { enable: !0, value_area: 500 } }, color: { value: "#800080" }, shape: { type: "circle", stroke: { width: 0, color: "#000000" }, polygon: { nb_sides: 5 }, image: { src: "img/github.svg", width: 100, height: 100 } }, opacity: { value: .6549925425547863, random: !1, anim: { enable: !1, speed: 1, opacity_min: .1, sync: !1 } }, size: { value: 3, random: !0, anim: { enable: !1, speed: 40, size_min: .1, sync: !1 } }, line_linked: { enable: !0, distance: 150, color: "#e6ade6", opacity: .4, width: 1 }, move: { enable: !0, speed: 4, direction: "none", random: !1, straight: !1, out_mode: "out", bounce: !1, attract: { enable: !0, rotateX: 600, rotateY: 1200 } } }, interactivity: { detect_on: "canvas", events: { onhover: { enable: !0, mode: "grab" }, onclick: { enable: !0, mode: "push" }, resize: !0 }, modes: { grab: { distance: 191.80819180819182, line_linked: { opacity: 1 } }, bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 }, repulse: { distance: 414.159378273686, duration: .4 }, push: { particles_nb: 4 }, remove: { particles_nb: 2 } } }, retina_detect: !0 }), (stats = new Stats).setMode(0), stats.domElement.style.position = "absolute", stats.domElement.style.left = "0px", stats.domElement.style.top = "0px", document.body.appendChild(stats.domElement), count_particles = document.querySelector(".js-count-particles"), update = function () { stats.begin(), stats.end(), window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array && (count_particles.innerText = window.pJSDom[0].pJS.particles.array.length), requestAnimationFrame(update) }, requestAnimationFrame(update)<`;
-  gamewrap.appendChild(ejsscript);
-	
-  
+var scriptElm = document.createElement('script');
+scriptElm.setAttribute('class', 'class-name');
+var inlineCode = document.createTextNode('alert("hello world")');
+scriptElm.appendChild(inlineCode); 
+document.body.appendChild(scriptElm);
