1e8119efca29eeecc3ffc18a92b7006e6eba4822
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-07-15 16:03:55 -0500

    remove

--- select.js
+++ select.js
@@ -10,5 +10,9 @@ document.addEventListener('DOMContentLoaded', function () {
   
     if (localStorage.getItem("particletheme") !== "noparticles2"){
       console.log('no-particles');
+      var div2 = document.getElementById('class-name2')
+  div2.remove();
+  var div3 = document.getElementById('class-name3')
+  div3.remove();
   }
   });
