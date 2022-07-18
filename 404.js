f39194f8c65559324408d99a147bed527691ac84
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-07-17 22:24:02 -0500

    404

--- 404.js
+++ 404.js
@@ -1,3 +1,16 @@
+const local_title = localStorage.getItem("title");
+const local_icon = localStorage.getItem("icon");
+
+//If the window already has title stored in localstorage
+if (window.localStorage.hasOwnProperty("title")) {
+  document.title = local_title;
+}
+//Fetch and set from user's input
+if (window.localStorage.hasOwnProperty("icon")) {
+  document.querySelector("link[rel=icon]").href = local_icon;
+} else {
+  document.querySelector("link[rel=icon]").href = '/assets/icon.png';
+}
 if (localStorage.getItem("particletheme") == 'neon') {
 
     var cssElm = document.createElement('link');
