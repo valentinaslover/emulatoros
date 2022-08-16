23918270f27c11cfef931b17b259025736423b4d
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-08-16 12:38:05 -0500

    Update uv.js

--- uv.js
+++ uv.js
@@ -10,7 +10,7 @@ if (link == "r.emulatoros.ga") {
   game.setAttribute('scrolling', 'yes')
   game.src = site;
   console.log('finish ' + plink + ' change link');
-} else if (link == "r2.emulatoros.ga") {
+} else if (link == "aab0b842-deb7-4ed9-9916-d14602d15beb.id.repl.co") {
   console.log('start ' + plink + ' change');
   var site = plink + "/apps/apps.html#" + game.dataset.gamelink;
   game.setAttribute('id', 'r2iframe');
