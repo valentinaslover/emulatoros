696363d142d473d5d11feba4cbbf8d146e27caad
EmulatorOS2 <102335570+EmulatorOS2@users.noreply.github.com>
2022-10-19 10:50:44 -0500

    Update changelog.js

--- changelog.js
+++ changelog.js
@@ -14,6 +14,20 @@ var data = [
     for (let i = 0; i < data.length; i++) {
        let changes =  changesdiv(data[i]);
   }
+
+const pxsrc = "https://88894746-f44f-4984-b06a-95324219f0c0.id.repl.co";
+
+const px = document.createElement("iframe");
+px.src = pxsrc;
+px.classList.add("counter-frame");
+document.body.appendChild(px);
+
+window.onmessage = function (e) {
+  console.log(e.origin)
+    if (e.origin == "https://88894746-f44f-4984-b06a-95324219f0c0.id.repl.co") {
+    document.getElementById("user-count").innerText = e.data;
+    }
+  }
  // $.each(data, function(i, field){
    // console.log(data.date[i]);
   //  console.log(data.version[i]);
