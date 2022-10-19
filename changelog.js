bc1b87e798156cb9e1c361e0be5447e4ba651f9f
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-10-18 21:25:43 -0500

    testing

--- changelog.js
+++ changelog.js
@@ -1,7 +1,11 @@
 $.getJSON('/changelog.json', (data) => {
 $.each(data, function(field){
-    console.log(data)
-    console.log(field)
+    console.log(data.date);
+    console.log(data.version);
+    $.each(data.changes, function (change) { 
+         console.log(change.type)
+         console.log(change.desc)
+    });
     const main = document.createElement('div');
     $('changelog').appendTo(main);
   });
