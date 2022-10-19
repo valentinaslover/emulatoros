672f0c4f334bdcb59787913debd2b3d31f244189
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-10-19 08:12:11 -0500

    test

--- changelog.js
+++ changelog.js
@@ -9,7 +9,7 @@ var data = [
         }
     ]
     const changesdiv = (change) => {
-        console.log(change.version, change.date, change.changes.type,  change.changes.desc)
+        console.log(change.version, change.date, change.changes.length,  change.changes.desc)
     }
     for (let i = 0; i < data.length; i++) {
        let changes =  changesdiv(data[i]);
