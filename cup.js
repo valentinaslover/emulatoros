92f1c32c89ab8b3cb969ebe5a62b939d2c3b751c
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-11-12 17:52:35 -0600

    oops

--- cup.js
+++ cup.js
@@ -52,37 +52,37 @@
     var app = document.getElementsByClassName("app")[app]
     
     var maindiv = document.createElement("div")
-    maindiv.setAttribute("style", 'div-child-box bg-dark-gray  bg-white py-2 position-relative') 
+    maindiv.setAttribute("class", 'div-child-box bg-dark-gray  bg-white py-2 position-relative') 
     app.appendChild(maindiv)
     
     var maindiv2 = document.createElement("div")
-    maindiv2.setAttribute("style", 'd-flex justify-content-center row') 
+    maindiv2.setAttribute("class", 'd-flex justify-content-center row') 
     maindiv.appendChild(maindiv2)
 
     var team1div = document.createElement("div")
-    team1div.setAttribute("style", 'col-4 text-right') 
+    team1div.setAttribute("class", 'col-4 text-right') 
     maindiv2.appendChild(team1div)
 
     var team1span = document.createElement("span")
-    team1span.setAttribute("style", 'd-inline-block mt-2') 
+    team1span.setAttribute("class", 'd-inline-block mt-2') 
     team1span.innerText = team1
     team1div.appendChild(team1span)
 
     var datediv = document.createElement("div")
-    datediv.setAttribute("style", 'col-3 text-center') 
+    datediv.setAttribute("class", 'col-3 text-center') 
     maindiv2.appendChild(datediv)
 
     var datespan = document.createElement("span")
-    datespan.setAttribute("style", 'd-inline-block text-center text-dark-l') 
+    datespan.setAttribute("class", 'd-inline-block text-center text-dark-l') 
     datespan.innerHTML = '<i class="fas fa-clock"></i><br>' + date + '&nbsp;&nbsp;' + time
     datediv.appendChild(datespan)
 
     var team2div = document.createElement("div")
-    team2div.setAttribute("style", 'col-5 text-left') 
+    team2div.setAttribute("class", 'col-5 text-left') 
     maindiv2.appendChild(team2div)
 
     var team2span = document.createElement("span")
-    team2span.setAttribute("style", 'd-inline-block mt-2') 
+    team2span.setAttribute("class", 'd-inline-block mt-2') 
     team2span.innerText = team2
     team2div.appendChild(team2span)
     
