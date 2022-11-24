6011e1f084f4e22f7503a6c43b7db21724cb41ae
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-11-24 10:16:15 -0600

    test

--- main.js
+++ main.js
@@ -332,6 +332,15 @@ function searchGames() {
       : a[i].setAttribute("style", "display:none !important");
 }
 
-$(".fa-star").click(function () {
-  $(this).toggleClass("fas far");
+$('#theModal').on('show.bs.modal', function (e) {
+
+  var button = $(e.relatedTarget);
+  var modal = $(this);
+
+  // load content from HTML string
+  //modal.find('.modal-body').html("Nice modal body baby...");
+
+  // or, load content from value of data-remote url
+  modal.find('.modal-body').load(button.data("remote"));
+
 });
