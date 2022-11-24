2ec5d31258de3fee65aa45df9205fb2838c7a0af
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-11-24 09:53:58 -0600

    test

--- nohist.js
+++ nohist.js
@@ -4,3 +4,15 @@ myWindow.document.write("<iframe onload=\"\" id='iframe' width=\"100%\" height=\
 myWindow.document.write("<style>body { margin:0;}</style>")
 myWindow.document.write("<script>function home(){document.getElementById('iframe').src = \""+url+"\"}</script>")
 myWindow.document.close();myWindow.stop();window.location.replace('https://google.com');}
+$('#theModal').on('show.bs.modal', function (e) {
+
+    var button = $(e.relatedTarget);
+    var modal = $(this);
+
+    // load content from HTML string
+    //modal.find('.modal-body').html("Nice modal body baby...");
+
+    // or, load content from value of data-remote url
+    modal.find('.modal-body').load(button.data("remote"));
+
+});
