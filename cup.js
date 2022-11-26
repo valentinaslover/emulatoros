88455f4dca7010b21f8c09950f8bee5b4398a2b5
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-11-26 16:45:28 -0600

    test

--- cup.js
+++ cup.js
@@ -41,7 +41,7 @@ function opentab() {
 }
 var plink = localStorage.getItem("plink");
 async function fetchapps() {
-  let response = await fetch("https://qatar.up.railway.app/api?q=matches");
+  let response = await fetch("https://qatar.up.railway.app/api?q=matches/today");
   let json = await response.json();
   var main = document.getElementById("font-md-10");
   for (app in json) {
@@ -55,7 +55,71 @@ async function fetchapps() {
     var away = team2.name;
     console.log(away + " and " + home)
     var status = json[app].status;
-    if (status !== "completed") {
+    if (status !== "completed" && status !== "on_progress") {
+      console.log('before ' + status)
+      var appelm = document.createElement("a");
+
+      appelm.className = "app";
+
+      main.appendChild(appelm);
+
+
+      var maindiv = document.createElement("div");
+      maindiv.setAttribute("class", "div-child-box bg-dark-gray  bg-white py-2 position-relative");
+      maindiv.setAttribute("style", "cursor:default;")
+      appelm.appendChild(maindiv);
+
+      var maindiv2 = document.createElement("div");
+      maindiv2.setAttribute("class", "d-flex justify-content-center row");
+      maindiv.appendChild(maindiv2);
+
+      var team1div = document.createElement("div");
+      team1div.setAttribute("class", "col-4 text-right");
+      maindiv2.appendChild(team1div);
+
+      var team1span = document.createElement("span");
+      team1span.setAttribute("class", "d-inline-block mt-2");
+      team1span.innerText = home;
+      team1div.appendChild(team1span);
+
+      var datediv = document.createElement("div");
+      datediv.setAttribute("class", "col-4 text-center");
+      maindiv2.appendChild(datediv);
+
+      var datespan = document.createElement("span");
+      datespan.setAttribute("class", "d-inline-block text-center text-dark-l");
+      datespan.innerHTML = '<i class="fas fa-clock"></i><br>' + date + " CT";
+      datediv.appendChild(datespan);
+
+      var team2div = document.createElement("div");
+      team2div.setAttribute("class", "col-4 text-left");
+      maindiv2.appendChild(team2div);
+
+      var team2span = document.createElement("span");
+      team2span.setAttribute("class", "d-inline-block mt-2");
+      team2span.innerText = away;
+      team2div.appendChild(team2span);
+
+      
+    }
+  }
+}
+async function current() {
+  let response = await fetch("https://qatar.up.railway.app/api?q=matches/current");
+  let json = await response.json();
+  var main = document.getElementById("font-md-10");
+  for (app in json) {
+
+    var format = moment(json[app].datetime);
+    var date = format.tz('America/Chicago').format('llll');
+    console.log(date)
+    var team1 = json[app].home_team;
+    var team2 = json[app].away_team;
+    var home = team1.name;
+    var away = team2.name;
+    console.log(away + " and " + home)
+    var status = json[app].status;
+    
       console.log('before ' + status)
       var appelm = document.createElement("a");
 
@@ -135,9 +199,8 @@ async function fetchapps() {
       best.setAttribute("onclick", "window.open('https://qatar.up.railway.app/apps/apps.html#https://v4.sportsonline.to/channels/hd/hd1.php')");
       streamsdiv.appendChild(best);
 
-    }
+    
   }
 }
-
-
+current();
 fetchapps();
