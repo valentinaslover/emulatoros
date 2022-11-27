4c12f1c345d4ed86324ff7162b266f5978fb494a
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-11-26 20:29:37 -0600

    previous

--- cup.js
+++ cup.js
@@ -177,5 +177,83 @@ async function current() {
 
   }
 }
+
+async function previous() {
+  let response = await fetch("https://qatar.up.railway.app/api?q=matches");
+  let json = await response.json();
+  var main = document.getElementById("previous");
+  for (app in json) {
+
+    var format = moment(json[app].datetime);
+    var date = format.tz('America/Chicago').format('llll');
+    console.log(date)
+    var team1 = json[app].home_team;
+    var team2 = json[app].away_team;
+    var home = team1.name;
+    var away = team2.name;
+
+    var status = json[app].status;
+    console.log(`${away} is ${status}`)
+    if (status == "completed") {
+
+      var appelm = document.createElement("a");
+
+      appelm.className = "app";
+
+      main.appendChild(appelm);
+
+
+      var maindiv = document.createElement("div");
+      maindiv.setAttribute("class", "div-child-box bg-dark-gray  bg-white py-2 position-relative");
+      maindiv.setAttribute("style", "cursor:default;")
+      appelm.appendChild(maindiv);
+
+      var maindiv2 = document.createElement("div");
+      maindiv2.setAttribute("class", "d-flex justify-content-center row");
+      maindiv.appendChild(maindiv2);
+
+      var team1div = document.createElement("div");
+      team1div.setAttribute("class", "d-flex justify-content-center align-items-center col-4 text-right");
+      maindiv2.appendChild(team1div);
+
+      var team1img = document.createElement("img");
+      team1img.setAttribute("class", "p-2 d-inline-block  ");
+      team1img.src = getFlags(team1.country);
+      team1div.appendChild(team1img);
+
+      var team1span = document.createElement("span");
+      team1span.setAttribute("class", "p-2 d-inline-block  text-center");
+      team1span.innerText = home;
+      team1div.appendChild(team1span);
+
+      var datediv = document.createElement("div");
+      datediv.setAttribute("class", "d-flex justify-content-center align-items-center col-4 text-center");
+      maindiv2.appendChild(datediv);
+
+      var datespan = document.createElement("span");
+      datespan.setAttribute("class", "p-2 d-inline-block text-center text-dark-l");
+      datespan.innerHTML = '<i class="fas fa-clock"></i><br>' + date + " CT";
+      datediv.appendChild(datespan);
+
+      var team2div = document.createElement("div");
+      team2div.setAttribute("class", "d-flex justify-content-center align-items-center col-4 text-left");
+      maindiv2.appendChild(team2div);
+
+      
+
+      var team2img = document.createElement("img");
+      team2img.setAttribute("class", "p-2 d-inline-block  ");
+      team2img.src = getFlags(team2.country);
+      team2div.appendChild(team2img);
+
+      var team2span = document.createElement("span");
+      team2span.setAttribute("class", "p-2 d-inline-block text-center ");
+      team2span.innerText = away;
+      team2div.appendChild(team2span);
+
+    }
+  }
+}
 current();
 fetchapps();
+previous();
