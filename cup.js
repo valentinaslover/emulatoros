e74aa36c89fee733ba37b519e0d5af54fce76e26
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-11-26 18:21:31 -0600

    img

--- cup.js
+++ cup.js
@@ -1,5 +1,7 @@
 
-
+$.getScript('./flags.js', function(){
+  console.log( "Load was performed." );
+})
 function getFlags(code) {
   return flags[code]
 }
@@ -23,7 +25,6 @@ async function fetchapps() {
     if (status !== "completed" && status !== "on_progress" && home !== "To Be Determined") {
 
 
-      console.log(team1.country)
       console.log(getFlags(team1.country))
       var appelm = document.createElement("a");
 
@@ -45,6 +46,11 @@ async function fetchapps() {
       team1div.setAttribute("class", "col-4 text-right");
       maindiv2.appendChild(team1div);
 
+      var team1img = document.createElement("img");
+      team1img.setAttribute("class", "d-inline-block mt-2");
+      team1img.src = getFlags(team1.country);
+      team1div.appendChild(team1img);
+
       var team1span = document.createElement("span");
       team1span.setAttribute("class", "d-inline-block mt-2");
       team1span.innerText = home;
@@ -62,6 +68,11 @@ async function fetchapps() {
       var team2div = document.createElement("div");
       team2div.setAttribute("class", "col-4 text-left");
       maindiv2.appendChild(team2div);
+
+      var team2img = document.createElement("img");
+      team2img.setAttribute("class", "d-inline-block mt-2");
+      team2img.src = getFlags(team2.country);
+      team2div.appendChild(team2img);
 
       var team2span = document.createElement("span");
       team2span.setAttribute("class", "d-inline-block mt-2");
