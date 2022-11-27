971601870a083cb4195f05b67f96f76ed3664099
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-11-27 00:21:13 -0600

    test

--- stats.js
+++ stats.js
@@ -17,9 +17,11 @@ function interval() {
     setInterval(update, 1000);
 }
 
-async function current() {
-    let response = await fetch("https://qatar.up.railway.app/api?q=matches/" + id);
-    let json = await response.json();
+function update() {
+$(function () {
+    $.getJSON(
+        "https://qatar.up.railway.app/api?q=matches/" + id,
+        function (json) {
     var format = moment(json.datetime);
     var date = format.tz('America/Chicago').format('llll');
     var main = document.getElementById('main');
@@ -36,76 +38,7 @@ async function current() {
     var appelm = document.createElement("div");
 
     appelm.className = "live";
-
-    main.prepend(appelm);
-
-
-    var maindiv = document.createElement("div");
-    maindiv.setAttribute("class", "div-child-box bg-dark-gray  bg-white py-2 position-relative");
-    maindiv.setAttribute("style", "cursor:default;")
-    appelm.appendChild(maindiv);
-
-
-    var maindiv2 = document.createElement("div");
-    maindiv2.setAttribute("class", "d-flex justify-content-center row");
-    maindiv.appendChild(maindiv2);
-
-    var team1div = document.createElement("div");
-    team1div.setAttribute("class", "d-flex d-flex justify-content-center align-items-center col-4 text-right");
-    maindiv2.appendChild(team1div);
-    var team1img = document.createElement("img");
-    team1img.setAttribute("class", "p-2 d-inline-block  ");
-    team1img.src = getFlags(team1.country);
-    team1div.appendChild(team1img);
-    var team1span = document.createElement("span");
-    team1span.setAttribute("class", "p-2 d-inline-block  text-center");
-    team1span.innerText = home;
-    team1div.appendChild(team1span);
-
-
-
-    var datediv = document.createElement("div");
-    datediv.setAttribute("class", "d-flex justify-content-center align-items-center col-4 text-center");
-    maindiv2.appendChild(datediv);
-    if (status == 'in_progress') {
-        var datespan = document.createElement("span");
-        datespan.setAttribute("class", "p-2 d-inline-block text-center text-dark-l");
-        datespan.innerHTML = ' Live <span class="live-icon"></span>';
-        datediv.appendChild(datespan);
-        interval(json)
-    } else {
-        var datespan = document.createElement("span");
-        datespan.setAttribute("class", "p-2 d-inline-block text-center text-dark-l");
-        datespan.innerHTML = 'Played on<br>' + date + " CT";
-        datediv.appendChild(datespan);
-    }
-    var team2div = document.createElement("div");
-    team2div.setAttribute("class", "d-flex justify-content-center align-items-center col-4 text-left");
-    maindiv2.appendChild(team2div);
-    var team2img = document.createElement("img");
-    team2img.setAttribute("class", "p-2 d-inline-block  ");
-    team2img.src = getFlags(team2.country);
-    team2div.appendChild(team2img);
-
-    var team2span = document.createElement("span");
-    team2span.setAttribute("class", "p-2 d-inline-block  text-center");
-    team2span.innerText = away;
-    team2div.appendChild(team2span);
-    var score = document.createElement("span");
-    score.setAttribute("class", "h3 d-flex justify-content-center text-center");
-    score.setAttribute("id", "score");
-    score.innerText = team1.goals + ' Goals ' + team2.goals;
-    maindiv.appendChild(score);
-    try {
-        var shots = document.createElement("span");
-        shots.setAttribute("class", "h3 d-flex justify-content-center text-center");
-        shots.setAttribute("id", "score");
-        shots.innerText = homestats.attempts_on_goal + ' Shots ' + awaystats.attempts_on_goal;
-        maindiv.appendChild(shots);
-    }
-    catch (err) {
-        console.log(err)
-    }
+    
     appelm.innerHTML =
         `<table>
   <tr>
@@ -166,8 +99,8 @@ async function current() {
 </table>`
 
     // check if playing
-
-
+    main.prepend(appelm);
+});
+});
 }
-
-current();
+interval();
