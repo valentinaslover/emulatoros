44cebafb47f8a2c6b5237b3b10050e12b5465619
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-11-26 18:39:18 -0600

    img style

--- cup.js
+++ cup.js
@@ -1,6 +1,6 @@
 
-$.getScript('./flags.js', function(){
-  console.log( "Load was performed." );
+$.getScript('./flags.js', function () {
+  console.log("Load was performed.");
 })
 function getFlags(code) {
   return flags[code]
@@ -69,47 +69,47 @@ async function fetchapps() {
       team2div.setAttribute("class", "col-4 text-left");
       maindiv2.appendChild(team2div);
 
-      var team2img = document.createElement("img");
-      team2img.setAttribute("class", "d-inline-block mt-2");
-      team2img.src = getFlags(team2.country);
-      team2div.appendChild(team2img);
-
       var team2span = document.createElement("span");
       team2span.setAttribute("class", "d-inline-block mt-2");
       team2span.innerText = away;
       team2div.appendChild(team2span);
 
+      var team2img = document.createElement("img");
+      team2img.setAttribute("class", "d-inline-block mt-2");
+      team2img.src = getFlags(team2.country);
+      team2div.appendChild(team2img);
+
       var streamsdiv = document.createElement("div");
-    streamsdiv.setAttribute("class", "d-flex justify-content-center");
-    maindiv.appendChild(streamsdiv);
+      streamsdiv.setAttribute("class", "d-flex justify-content-center");
+      maindiv.appendChild(streamsdiv);
 
-    var stream = document.createElement("button");
-    stream.setAttribute("class", "btn btn-sm btn-danger m-2");
-    stream.innerText = "Stream 1(EN)";
-    stream.setAttribute("onclick", "window.open('https://qatar.up.railway.app/apps/apps.html#https://techclips.net/clip/s1.html')");
-    streamsdiv.appendChild(stream);
+      var stream = document.createElement("button");
+      stream.setAttribute("class", "btn btn-sm btn-danger m-2");
+      stream.innerText = "Stream 1(EN)";
+      stream.setAttribute("onclick", "window.open('https://qatar.up.railway.app/apps/apps.html#https://techclips.net/clip/s1.html')");
+      streamsdiv.appendChild(stream);
 
-    var stream2 = document.createElement("button");
-    stream2.setAttribute("class", "btn btn-sm btn-danger m-2");
-    stream2.innerText = "Stream 2(EN)";
-    stream2.setAttribute("onclick", "window.open('https://qatar.up.railway.app/apps/apps.html#https://l1l1.to/ch4')");
-    streamsdiv.appendChild(stream2);
+      var stream2 = document.createElement("button");
+      stream2.setAttribute("class", "btn btn-sm btn-danger m-2");
+      stream2.innerText = "Stream 2(EN)";
+      stream2.setAttribute("onclick", "window.open('https://qatar.up.railway.app/apps/apps.html#https://l1l1.to/ch4')");
+      streamsdiv.appendChild(stream2);
 
-    var stream3 = document.createElement("button");
-    stream3.setAttribute("class", "btn btn-sm btn-danger m-2");
-    stream3.innerText = "Stream 3(ES)";
-    stream3.setAttribute("onclick", "window.open('https://qatar.up.railway.app/apps/apps.html#https://l1l1.to/ch10')");
-    streamsdiv.appendChild(stream3);
-    var stream4 = document.createElement("button");
-    stream4.setAttribute("class", "btn btn-sm btn-danger m-2");
-    stream4.innerText = "Stream 4(EN)";
-    stream4.setAttribute("onclick", "window.open('https://qatar.up.railway.app/apps/apps.html#https://liveon.sx/embed1')");
-    streamsdiv.appendChild(stream4);
-    var best = document.createElement("button");
-    best.setAttribute("class", "btn btn-sm btn-danger m-2");
-    best.innerText = "Best but Laggy(EN)";
-    best.setAttribute("onclick", "window.open('https://qatar.up.railway.app/apps/apps.html#https://v4.sportsonline.to/channels/hd/hd1.php')");
-    streamsdiv.appendChild(best);
+      var stream3 = document.createElement("button");
+      stream3.setAttribute("class", "btn btn-sm btn-danger m-2");
+      stream3.innerText = "Stream 3(ES)";
+      stream3.setAttribute("onclick", "window.open('https://qatar.up.railway.app/apps/apps.html#https://l1l1.to/ch10')");
+      streamsdiv.appendChild(stream3);
+      var stream4 = document.createElement("button");
+      stream4.setAttribute("class", "btn btn-sm btn-danger m-2");
+      stream4.innerText = "Stream 4(EN)";
+      stream4.setAttribute("onclick", "window.open('https://qatar.up.railway.app/apps/apps.html#https://liveon.sx/embed1')");
+      streamsdiv.appendChild(stream4);
+      var best = document.createElement("button");
+      best.setAttribute("class", "btn btn-sm btn-danger m-2");
+      best.innerText = "Best but Laggy(EN)";
+      best.setAttribute("onclick", "window.open('https://qatar.up.railway.app/apps/apps.html#https://v4.sportsonline.to/channels/hd/hd1.php')");
+      streamsdiv.appendChild(best);
 
     }
   }
