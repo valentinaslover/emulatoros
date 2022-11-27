e4266c22e4c2cb2cb6ae562f678b6f2355f8cb2b
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-11-27 15:48:10 -0600

    test

--- cup.js
+++ cup.js
@@ -4,7 +4,37 @@ function getFlags(code) {
   var flags = "https://qatar.up.railway.app/flags?q=" + code
   return flags
 }
+function dot(finalDot){
+  return (
+    <svg style={{width: '100%', height: '48px'}}>
+        <rect x="40%" y={finalDot ? '30%' : '0%'} width="20%" height="100%" fill="#2D3748" />
+        <circle cx="50%" cy="30%" r="10%" fill="#2D3748" />
+    </svg>
+)
+}
 function PlayByPlay(code) {
+  const events = [
+    ...match.home_team_events.map(event => ({...event, country:match.home_team_country})), 
+    ...match.away_team_events.map(event => ({...event, country:match.away_team_country}))
+]
+events.sort((a,b) => b.id - a.id)
+
+return (
+    <Grid maxHeight={'40vh'} gridTemplateColumns={'1fr 1em 1fr'} overflowY={'scroll'}>
+        {events.map((event, i) => (
+            <>
+                {event.country === match.home_team_country && <><Text key={`time_${event.id}`} textAlign={'right'}>{event.time}</Text><Dot key={`dot_${event.id}`} finalDot={i === 0}/></>}
+                <Box key={event.id} textAlign={event.country === match.home_team_country ? 'left' : 'right'}>
+                    <Badge colorScheme={eventToColour(event.type_of_event)}>
+                        {mapEventType(event.type_of_event)}
+                    </Badge>
+                    <Text>{event.player}</Text>
+                </Box>
+                {event.country !== match.home_team_country && <><Dot key={`dot_${event.id}`} finalDot={ i === 0 }/><Text key={`time_${event.id}`}>{event.time}</Text></>}
+            </>
+        ))}
+    </Grid>
+)
   console.log(hi)
 }
 function update() {
@@ -23,8 +53,8 @@ function update() {
         // Patching payload into page element ID = "dog" 
       });
 }
-function interval() {
-  PlayByPlay();
+function interval(app, play) {
+  play.innerHTML = PlayByPlay(app);
   setInterval(update, 10000);
 }
 
@@ -128,7 +158,7 @@ async function current() {
 
     // 1fr 1em 1fr
     var play = document.createElement("div");
-    play.setAttribute("class", "container");
+    play.setAttribute("class", "d-grid play");
     play.setAttribute("style", "cursor:default;")
     appelm.appendChild(play);
     play.innerHTML = `play`
@@ -226,7 +256,7 @@ async function current() {
     streamsdiv.appendChild(best);
     
 
-    interval();
+    interval(app, play);
   }
 }
 
