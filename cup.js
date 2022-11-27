3b3053ae8d87aa2cfc0e4514c4bc32e88617ec4e
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-11-27 14:46:07 -0600

    log

--- cup.js
+++ cup.js
@@ -9,6 +9,9 @@ function update(app) {
     $.getJSON(
       "https://qatar.up.railway.app/api?q=matches/current",
       function (json) {
+        console.log(json[app])
+        console.log(json[app].home_team)
+        console.log(json[app].home_team.goals)
         $('#score').text(json[app].home_team.goals + ' - ' + json[app].away_team.goals);
         // Patching payload into page element ID = "dog" 
       });
