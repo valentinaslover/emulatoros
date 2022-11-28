81aa82cd31e3c5eec85aa9970503e2d9bb03b5e1
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-11-27 19:44:17 -0600

    fix

--- cup.js
+++ cup.js
@@ -54,13 +54,24 @@ function interval(app, play, json) {
     if (events[app].country === home ) {
      
     var eventelm = document.createElement("div");
-    console.log(home + ' and ' + events[app].type_of_event)
-    play.createElement = `<div style="text-align:right;">
-    <span class="type"style="color:${eventToColour(events[app].type_of_event)}">${mapEventType(events[app].type_of_event)}</span>
-    <p>${events[app].player}</p>
-    </div><svg style="width: 100%; height: 48px;"><rect x="40%" y="30%" width="20%" height="100%" fill="#2D3748"></rect><circle cx="50%" cy="30%" r="10%" fill="#2D3748"></circle></svg>
-    <p style="margin: 0;">${events[app].time}</p>`
-    play.prepend(eventelm);
+    eventelm.setAttribute("style", "text-align:right;")
+      play.appendChild(eventelm);
+      eventelm.innerHTML = `<span class="type"style="color:${eventToColour(events[app].type_of_event)}">${mapEventType(events[app].type_of_event)}</span>
+      <p>${events[app].player}</p>`
+
+      var svg = document.createElement("div");
+    svg.setAttribute("style", "width: 100%; height: 48px;")
+      play.appendChild(svg);
+      svg.innerHTML = ` 
+      <rect x="40%" y="30%" width="20%" height="100%" fill="#2D3748"></rect>
+      <circle cx="50%" cy="30%" r="10%" fill="#2D3748"></circle>`
+      
+      var p = document.createElement("div");
+      p.setAttribute("style", "margin:0;")
+      p.innerHTML = ` 
+      <rect x="40%" y="30%" width="20%" height="100%" fill="#2D3748"></rect>
+      <circle cx="50%" cy="30%" r="10%" fill="#2D3748"></circle>`
+      play.appendChild(p);
   } else {
     var eventelm = document.createElement("div");
     console.log(away + ' else ' + events[app].type_of_event)
