74ecc6cf17356fc17d1cc7ca886f9cbeafc8f8b3
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-11-27 18:57:13 -0600

    fix

--- cup.js
+++ cup.js
@@ -55,10 +55,7 @@ function interval(app, play, json) {
      
     var eventelm = document.createElement("div");
     console.log(home + ' and ' + events[app].type_of_event)
-    eventelm.innerHTML = `<p class="text-align-right">${events[app].time}</p><svg style="width: 100%; height: 48px">
-    <rect x="40%" y=${app ? '30%' : '0%'} width="20%" height="100%" fill="#2D3748" />
-    <circle cx="50%" cy="30%" r="10%" fill="#2D3748" />
-</svg><div style="text-align:${events[app].country === home ? 'left' : 'right'};">
+    eventelm.innerHTML = `<p class="text-align-right">${events[app].time}</p><svg style="width: 100%; height: 48px;"><rect x="40%" y="30%" width="20%" height="100%" fill="#2D3748"></rect><circle cx="50%" cy="30%" r="10%" fill="#2D3748"></circle></svg><div style="text-align:${events[app].country === home ? 'left' : 'right'};">
 <p style="color:${eventToColour(events[app].type_of_event)}">${mapEventType(events[app].type_of_event)}</p>
 <p>${events[app].player}</p>
 </div>`
