3131ce575aded5b983a297256aeb347c3f8c5c50
EmulatorOS2 <102335570+EmulatorOS2@users.noreply.github.com>
2022-11-29 14:37:49 -0600

    Update cup.js

--- cup.js
+++ cup.js
@@ -415,7 +415,7 @@ $(document).ready(function() {
   fetchapps();
   previous();
   current();
-  const check = document.getElementsById('stream1')
+  const check = $('#stream1');
 console.log(check)
   console.log('load4')
 if (check === null) {
