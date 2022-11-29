6e2139a3e3c6b7ae1fd7da5dbf0b07d8c28b50cb
EmulatorOS2 <102335570+EmulatorOS2@users.noreply.github.com>
2022-11-29 11:28:04 -0600

    Update cup.js

--- cup.js
+++ cup.js
@@ -116,6 +116,7 @@ async function fetchapps() {
     var home = team1.name;
     var away = team2.name;
 
+    var previous = document.getElementById('live');
     var status = json[app].status;
     if (status !== "completed" && status !== "in_progress" && home !== "To Be Determined") {
 
@@ -174,7 +175,74 @@ async function fetchapps() {
       team2span.innerText = away;
       team2div.appendChild(team2span);
 
-    }
+    } else if (status == "completed") {
+
+      var appelm = document.createElement("a");
+
+      appelm.className = "previous";
+
+      previous.prepend(appelm);
+
+
+      var maindiv = document.createElement("div");
+      maindiv.setAttribute("class", "div-child-box bg-dark-gray  bg-white py-2 d-flex flex-column align-items-center");
+      maindiv.setAttribute("style", "cursor:default;")
+      appelm.appendChild(maindiv);
+
+      var score = document.createElement("span");
+      score.setAttribute("class", "h3 d-flex justify-content-center text-center");
+      score.innerText = team1.goals + ' - ' + team2.goals;
+      maindiv.appendChild(score);
+
+      var maindiv2 = document.createElement("div");
+      maindiv2.setAttribute("class", "d-flex justify-content-center row");
+      maindiv.appendChild(maindiv2);
+
+      var team1div = document.createElement("div");
+      team1div.setAttribute("class", "d-flex justify-content-center align-items-center col-4 text-right");
+      maindiv2.appendChild(team1div);
+
+      var team1img = document.createElement("img");
+      team1img.setAttribute("class", "p-2 d-inline-block  ");
+      team1img.src = getFlags(team1.country);
+      team1div.appendChild(team1img);
+
+      var team1span = document.createElement("span");
+      team1span.setAttribute("class", "p-2 d-inline-block  text-center");
+      team1span.innerText = home;
+      team1div.appendChild(team1span);
+
+      var datediv = document.createElement("div");
+      datediv.setAttribute("class", "d-flex justify-content-center align-items-center col-4 text-center");
+      maindiv2.appendChild(datediv);
+
+
+      var datespan = document.createElement("span");
+      datespan.setAttribute("class", "p-2 d-inline-block text-center text-dark-l");
+      datespan.innerHTML = 'Played on<br>' + date + " CT";
+      datediv.appendChild(datespan);
+
+      var team2div = document.createElement("div");
+      team2div.setAttribute("class", "d-flex justify-content-center align-items-center col-4 text-left");
+      maindiv2.appendChild(team2div);
+
+
+
+      var team2img = document.createElement("img");
+      team2img.setAttribute("class", "p-2 d-inline-block  ");
+      team2img.src = getFlags(team2.country);
+      team2div.appendChild(team2img);
+
+      var team2span = document.createElement("span");
+      team2span.setAttribute("class", "p-2 d-inline-block text-center ");
+      team2span.innerText = away;
+      team2div.appendChild(team2span);
+      var stats = document.createElement("button");
+      stats.setAttribute("class", "p-2 d-inline-block btn btn-sm btn-danger");
+      stats.innerText = 'Stats';
+      stats.setAttribute("onclick", `window.location.href='/stats#${json[app].id}'`);
+      maindiv.appendChild(stats);
+
   }
 
 }
@@ -185,7 +253,6 @@ async function current() {
 
   for (app in json) {
 
-    var main = document.getElementById('live');
     var team1 = json[app].home_team;
     var team2 = json[app].away_team;
     var home = team1.name;
@@ -304,99 +371,13 @@ async function current() {
   }
 }
 
-async function previous() {
-  let response = await fetch("https://emulatoros.up.railway.app/api?q=matches");
-  let json = await response.json();
-  var main = document.getElementById("previous");
-  for (app in json) {
-
-    var format = moment(json[app].datetime);
-    var date = format.tz('America/Chicago').format('llll');
-    var team1 = json[app].home_team;
-    var team2 = json[app].away_team;
-    var home = team1.name;
-    var away = team2.name;
-
-    var status = json[app].status;
-    if (status == "completed") {
-
-      var appelm = document.createElement("a");
-
-      appelm.className = "previous";
-
-      main.prepend(appelm);
-
-
-      var maindiv = document.createElement("div");
-      maindiv.setAttribute("class", "div-child-box bg-dark-gray  bg-white py-2 d-flex flex-column align-items-center");
-      maindiv.setAttribute("style", "cursor:default;")
-      appelm.appendChild(maindiv);
-
-      var score = document.createElement("span");
-      score.setAttribute("class", "h3 d-flex justify-content-center text-center");
-      score.innerText = team1.goals + ' - ' + team2.goals;
-      maindiv.appendChild(score);
-
-      var maindiv2 = document.createElement("div");
-      maindiv2.setAttribute("class", "d-flex justify-content-center row");
-      maindiv.appendChild(maindiv2);
-
-      var team1div = document.createElement("div");
-      team1div.setAttribute("class", "d-flex justify-content-center align-items-center col-4 text-right");
-      maindiv2.appendChild(team1div);
-
-      var team1img = document.createElement("img");
-      team1img.setAttribute("class", "p-2 d-inline-block  ");
-      team1img.src = getFlags(team1.country);
-      team1div.appendChild(team1img);
-
-      var team1span = document.createElement("span");
-      team1span.setAttribute("class", "p-2 d-inline-block  text-center");
-      team1span.innerText = home;
-      team1div.appendChild(team1span);
-
-      var datediv = document.createElement("div");
-      datediv.setAttribute("class", "d-flex justify-content-center align-items-center col-4 text-center");
-      maindiv2.appendChild(datediv);
-
-
-      var datespan = document.createElement("span");
-      datespan.setAttribute("class", "p-2 d-inline-block text-center text-dark-l");
-      datespan.innerHTML = 'Played on<br>' + date + " CT";
-      datediv.appendChild(datespan);
-
-      var team2div = document.createElement("div");
-      team2div.setAttribute("class", "d-flex justify-content-center align-items-center col-4 text-left");
-      maindiv2.appendChild(team2div);
-
-
-
-      var team2img = document.createElement("img");
-      team2img.setAttribute("class", "p-2 d-inline-block  ");
-      team2img.src = getFlags(team2.country);
-      team2div.appendChild(team2img);
-
-      var team2span = document.createElement("span");
-      team2span.setAttribute("class", "p-2 d-inline-block text-center ");
-      team2span.innerText = away;
-      team2div.appendChild(team2span);
-      var stats = document.createElement("button");
-      stats.setAttribute("class", "p-2 d-inline-block btn btn-sm btn-danger");
-      stats.innerText = 'Stats';
-      stats.setAttribute("onclick", `window.location.href='/stats#${json[app].id}'`);
-      maindiv.appendChild(stats);
-
-    }
-  }
-}
 current();
 setTimeout(() => {
   fetchapps();
-previous();
   if (document.querySelector('.stream1') !== null) {
     const streamatt = document.querySelector('.stream1');
     console.log(streamatt)
     streamatt.setAttribute("onclick", "window.open('https://emulatoros.up.railway.app/apps/apps.html#https://techclips.net/clip/s2.html')");
     
   }
-}, "2000")
+}, "5000")
